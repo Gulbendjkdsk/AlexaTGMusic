ec9fc5bfce6b1aa4fc0c2b8fea803f6fc35d7997
Gulbendjkdsk <144287923+Gulbendjkdsk@users.noreply.github.com>
2025-03-24 20:59:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ load_dotenv()
 # VARS
 
 get_queue = {}
-BOT_TOKEN = getenv("BOT_TOKEN")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN"), "7997882781:AAEwzFafUd6jpelIjxIHl60Z4T5aRuW9Rmk"))
+API_ID = int(getenv("API_ID", "24669465"))
+API_HASH = getenv("API_HASH"), "ef15199702ea76ebb2ebe4eca477ab60"))
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI"), "mongodb+srv://cenap526:cenappp526@cluster0.7onsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6623140842").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002662249469"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
@@ -22,17 +22,17 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/jankarikiduniya/TG-Music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
-if str(getenv("SUPPORT_CHANNEL")).strip() == "":
+if str(getenv("SUPPORT_CHANNEL")).strip() == "https://t.me/+WWX0t-5B509iMWE0":
     SUPPORT_CHANNEL = None
 else:
     SUPPORT_CHANNEL = str(getenv("SUPPORT_CHANNEL"))
-if str(getenv("SUPPORT_GROUP")).strip() == "":
+if str(getenv("SUPPORT_GROUP")).strip() == "@Turkiyelinki":
     SUPPORT_GROUP = None
 else:
     SUPPORT_GROUP = str(getenv("SUPPORT_GROUP"))
 
 
-if str(getenv("STRING_SESSION1")).strip() == "":
+if str(getenv("STRING_SESSION1")).strip() == "BAF4bRkAmZqo6k7_HacnAbxm2-wU1_sHt3eSYofTxeo9tHOWq1u55UYhDNwv-0FUp9LJdjFbK0ch7rUVupCa-vbUNIUXfUZLZy6h3y5xJzHrEWe_tF4rxYLo2PXI6MoECbw14_J49pt0djDq3hzqRP4rsFwWBFOK421w0-O9_0Evbs-Gg0Tj6HAC3k-lGFp3d8w8DGmMLmh-vyZZVv2bZSj1EQLPjKD0Pv8PCMKLYMT_-b81IBWNbLzJWuF9Q2JPhgGeuqkRgA6O-AbNVT_oGQBsAe8UxhAVlRz-Rs61inueDuOWmILQddH0snFFGTx53BHT5wiuwaHr38sWCg_uXdo7ER9fhwAAAAG_f8l8AA":
     STRING1 = str(None)
 else:
     STRING1 = str(getenv("STRING_SESSION1"))
